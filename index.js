2336d9f356735f52a1e409543d6cf507eb96657c
lilian manaa <lelian.ibraheem@gmail.com>
2022-10-22 19:52:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let name = "Sam"
+let email = "sam@sam.com"
+let div = document.createElement("div")
+function submitData( name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email
+        }),
+    })
+    .then(res => res.json())
+    .then(data => document.body.innerHTML = data.id)
+    .catch((error) => {
+        let message = 'Unauthorized Access'
+        document.body.innerHTML = message
+        console.error('Error:', error);
+      });
+}
+submitData( name, email )
+
